68eb8926d3373ebba2a17688ff441083a50b3ae5
Siegfried <siegfried.bozza@renuo.ch>
2023-02-19 09:27:31 +0100

    update express server

--- server.js
+++ server.js
@@ -1,28 +1,73 @@
 const express = require('express')
 const cors = require('cors')
-
-const ORIGIN = 'http://localhost:3000'
-const EXPRESS_BASE_URL = '/api'
+const allowedOrigins = ['http://localhost:3000']
 
 const app = express()
-
-// middleware: json parser
 app.use(express.json())
-// middleware: cors
-const allowedOrigins = [ORIGIN]
 app.use(cors({origin: allowedOrigins}))
 
-let notes = [
+let data = [
     { id: 1, content: 'note from express', important: true },
 ]
 
+const BASE_URL = '/api'
+
 // index
-app.get(EXPRESS_BASE_URL, (req, res) => {
-    try{
-        res.json(notes)
-    } catch(error) {
-        console.log(error)
+app.get(BASE_URL, (req, res) => {
+    res.json(data)
+})
+
+// show
+app.get(`${BASE_URL}/:id`, (req, res) => {
+    const id = parseInt(req.params.id)
+    const datum = data.find(d => d.id === id)
+    if (datum) {
+       res.json(datum)
+    } else {
+        res.redirect('/404')
     }
+})
+
+// create
+app.post(`${BASE_URL}`, (req, res) => {
+    const body = req.body
+    data = [...data, body]
+    res.json(data)
+})
+
+// update
+app.patch(`${BASE_URL}/:id`, (req, res) => {
+    const id = parseInt(req.params.id)
+    const body = req.params.body
+    let datum = data.find(d => d.id === id)
+    if (datum) {
+        datum = {...datum, ...body}
+        data = data.map(d => {
+            return d.id !== id ? d : datum
+        })
+        res.json(datum)
+    } else {
+        res.redirect('/404')
+    }
+})
+
+// delete
+app.delete(`${BASE_URL}/:id`, (req, res) => {
+    const id = parseInt(req.params.id)
+    const datum = data.find(d => d.id === id)
+    if (datum) {
+        data = data.filter(d => d.id !== id)
+        res.json((datum))
+    } else {
+        res.redirect('/404')
+    }
+})
+
+// error
+app.get(`/404`, (req, res) => {
+    res.send(`
+        <h1>oups...page not found</h1>
+    `)
 })
 
 const PORT = 3001;
